d284f8bb88914ee7a9c2548af9ff8b75ff656121
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 04:58:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  collection = [" ", 1, 2, 3, 4]
+  1.times do
+  while i < array.length
+    yield array
+  end
+  return i
 end
